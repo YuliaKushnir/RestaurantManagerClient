c79b2659ae831aa30235df85ebd11d26e5b481f7
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-05-05 10:22:43 +0300

    Added saving user into local storage

--- src/app/auth-components/login/login.component.ts
+++ src/app/auth-components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { AuthService } from '../../auth-services/auth-service/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StorageService } from '../../auth-services/storage-service/storage.service';
 
 @Component({
   selector: 'app-login',
@@ -28,7 +29,18 @@ export class LoginComponent {
   submitForm(){
     this.service.login(this.loginForm.value).subscribe((res)=>{
       console.log(res);
+      if(res.userId != null){
+        const user = {
+          id:res.userId,
+          role:res.userRole
+        }
+        console.log(user);
+        StorageService.saveToken(res.jwt);
+        StorageService.saveUser(user);
 
+      } else {
+        console.log("Wrong credentials")
+      }
     })
   }
 
