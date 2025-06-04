f2c6b7496cd70ee88f38ca59e70773533cfd9487
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-06-04 13:56:07 +0300

    Filled main page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { StorageService } from '../auth-services/storage-service/storage.service';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
 
+  constructor(){}
+
+  isLoggedIn(): boolean {
+    return StorageService.isUserLoggedIn();
+  }
 }
